5eb1e15fbb1faea6b09d2f41021b2d204cdf185b
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-08-29 14:24:50 +0200

    server port 5000 on heroku

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use((req, res) => {
   res.status(400).send('404 not found...');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port: 3000');
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running on port: 5000');
 });
