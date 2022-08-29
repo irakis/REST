cad2892280ce7e2a84a8cbfed66e3167a6b584ea
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-08-29 13:38:57 +0200

    new server port 8080 for heroku

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use((req, res) => {
   res.status(400).send('404 not found...');
 });
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server is running on port: 8080');
 });
