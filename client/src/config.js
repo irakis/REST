1533ceb557e65ced8d22735bc42f184ae1cf1c9f
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-10-01 20:37:29 +0200

    heroku www in config file

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
+export const API_URL = (process.env.NODE_ENV === 'production') ? 'https://concerts-app-rest.herokuapp.com/api' : 'http://localhost:8000/api';
